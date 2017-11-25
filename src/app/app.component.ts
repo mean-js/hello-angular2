b8d73a4a73d6119c76176c38529a6ddd24d862d6
santosh <santosh.ece06@gmail.com>
2017-11-25 15:53:01 +0530

    component and service. with *ngFor.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
+import {Person} from './datamodel/person';
+import {User} from './datamodel/user';
+import {PersonService} from './dataservice/person.service';
+import {UserService} from './dataservice/user.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  constructor(private personService: PersonService, private userService: UserService) {}
+
+  title = 'Affixus Systems Pvt Ltd.';
+  userList: User[] = this.userService.getUsers();
+  personList: Person[] = this.personService.getPersons();
+
+  selectUser(user) {
+    console.log(user);
+  }
+
+  selectPerson(person) {
+    console.log(person);
+  }
 }
